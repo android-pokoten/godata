f6849a8826f4059bb8e8bb1b91d30e14873238c6
pokoten <android.pokoten@gmail.com>
2026-06-03 22:09:47 +0900

    species_id が _normal で終わるデータは重複データと考えて除外するように修正

--- web/core/loader.py
+++ web/core/loader.py
@@ -3,6 +3,9 @@ import pandas as pd
 def load_species():
     df = pd.read_csv("data/species.csv")
 
+    # species_id の _normal 付きデータは除外する
+    df = df[~df["species_id"].str.contains("_normal", case=False, na=False)]
+
     for col in ["elitefast", "elitecharge", "evolves_to", "evolves_from"]:
         if col in df.columns:
             df[col] = df[col].fillna("")
